cb4c1383c3655c4b06f6010cbcba0810c8e401e8
Mayank Singla <mayanksingla139@gmail.com>
2026-03-29 00:26:42 +0530

    Mouse Actions and Keyboard Actions

---
+++ src/test/java/DatePicker/datePickerNextPrevButton.java
@@ -0,0 +1,51 @@
+package DatePicker;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
+public class datePickerNextPrevButton {
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("https://testautomationpractice.blogspot.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        // using sendkeys
+        WebElement datePicker1 = driver.findElement(By.xpath("//input[@id=\"datepicker\"]"));
+        datePicker1.sendKeys("03/08/2026");
+        datePicker1.sendKeys(Keys.ENTER);
+
+        // selecting date from calendar
+        WebElement datePicker2 = driver.findElement(By.xpath("//input[@id=\"txtDate\"]"));
+        datePicker2.click();
+        String year = "2026";
+        String month = "Jan";
+        String date = "3";
+        while(true){
+            String openMonth = driver.findElement(By.xpath("//select[@class = \"ui-datepicker-month\"]//option[@selected]")).getText();
+            String openYear = driver.findElement(By.xpath("//select[@class = \"ui-datepicker-year\"]//option[@selected]")).getText();
+            if(openYear.equalsIgnoreCase(year) && openMonth.equalsIgnoreCase(month)){
+                break;
+            }
+            // next button
+//            driver.findElement(By.xpath("//span[contains(text(), 'Next')]")).click();
+
+            // previous button
+            driver.findElement(By.xpath("//span[contains(text(), 'Prev')]")).click();
+        }
+        List<WebElement> dates = driver.findElements(By.xpath("//table[@class = \"ui-datepicker-calendar\"]//tr//a"));
+        for(WebElement dt:dates){
+            if(dt.getText().equalsIgnoreCase(date)){
+                dt.click();
+            }
+        }
+
+    }
+}
---
+++ src/test/java/MouseActions/MouseActions.java
@@ -0,0 +1,59 @@
+package MouseActions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.time.Duration;
+
+public class MouseActions {
+
+    public static void main(String[] args){
+
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("https://testautomationpractice.blogspot.com/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+
+        Actions action = new Actions(driver);
+        // Hover
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebElement btnPointMe = driver.findElement(By.xpath("//button[@class = \"dropbtn\"]"));
+        WebElement mobileOption = driver.findElement(By.xpath("//a[text() = \"Mobiles\"]"));
+
+        action.moveToElement(btnPointMe).build().perform();
+        mobileOption.click();
+        System.out.println("Mobile button got clicked");
+
+        // Right Click
+        WebElement txtApple = driver.findElement(By.xpath("//a[text() = \"Apple\"]"));
+        action.contextClick(txtApple).perform();
+        System.out.println("Right Clicked Performed");
+
+        // Double Click
+        WebElement btnCopyText = driver.findElement(By.xpath("//button[text() = \"Copy Text\"]"));
+        action.doubleClick(btnCopyText).perform();
+        WebElement inputField = driver.findElement(By.xpath("//input[@id = \"field2\"]"));
+        if(inputField.getAttribute("value").equalsIgnoreCase("Hello World!")){
+            System.out.println("Double Clicked Performed");
+        }else{
+            System.out.println("Double Clicked Not Performed");
+        }
+
+        // Drag and drop
+        WebElement dragElement = driver.findElement(By.xpath("//div[@id= \"draggable\"]"));
+        WebElement dropElement = driver.findElement(By.xpath("//div[@id= \"droppable\"]"));
+        action.dragAndDrop(dragElement, dropElement).perform();
+        if(driver.findElement(By.xpath("//p[text() = \"Dropped!\"]")).isDisplayed()){
+            System.out.println("Drag and Drop Performed");
+        }else{
+            System.out.println("Drag and Drop Not Performed");
+        }
+
+
+
+
+    }
+}
